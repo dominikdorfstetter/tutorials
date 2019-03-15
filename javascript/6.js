a26f8711e5f69a52b25c1254cc8ded6632b71b32
dorfid <dorfstetter@posteo.de>
2019-03-15 18:36:51 +0100

    fixed some wording

--- javascript/6.js
+++ javascript/6.js
@@ -8,27 +8,27 @@ let numbers = [1, 2, 3];
 // wird der Überschuss einfach ignoriert.
 let [a, b] = numbers;
 
-console.log("a: ", a);
-console.log("b: ", b);
+console.log('a: ', a);
+console.log('b: ', b);
 
 let [c, d, e, f] = numbers;
 
 // Ist das Array zu klein für alle Variablen im Array
 // sind die Überschüssigen 'undefined'
-console.log("f: ", f);
+console.log('f: ', f);
 
 // Durch den Slice-Parameter kommt der Überschuss als Array zurück
 let [g, ...h] = numbers;
 
-console.log("h: ", h);
+console.log('h: ', h);
 
 // Wird ein = WERT angegeben ist das der default Value einer Variablen
-let [i = "blah", j, k, l = "default"] = numbers;
+let [i = 'blah', j, k, l = 'default'] = numbers;
 
 // hat default
-console.log("i: ", i);
+console.log('i: ', i);
 // hat keinen
-console.log("l: ", l);
+console.log('l: ', l);
 
 let var1 = 1;
 let var2 = 2;
@@ -45,7 +45,7 @@ let [var3, , var4] = numbers;
 console.log(`var3 ${var3} var4 ${var4}`);
 
 // Weg um bei der Variablen Erstellung Zeit zu sparen!
-let [name, alter, adresse] = ["Dominik", 28, "irgendwo in Wien ;)"];
+let [name, alter, adresse] = ['Dominik', 28, 'Meidling'];
 
 console.log(`
 Hallo ich bin ${name} und bin ${alter} Jahre alt,
@@ -55,14 +55,14 @@ als Antwort: ${adresse}
 
 // Halt dich fest ;D Das selbe geht mit Objekten
 let obj = {
-  gametag: "Oliver420",
-  level: 26,
-  gameOfLife: () => {
-    console.log(`
-Das Game das mir die Lebenskraft aussaugt: 
-------------- League of Legends ----------
+   gametag: 'Oliver420',
+   level: 26,
+   gameOfLife: () => {
+      console.log(`
+Das Game das mir die Zeit raubt: 
+------ League of Legends -------
         `);
-  }
+   }
 };
 
 // Dann brauchst du eine geschwungene Klammer
