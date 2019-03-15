0250e7cdc4fd30989e773746a26654b360ec0538
dorfid <dorfstetter@posteo.de>
2019-03-15 18:53:55 +0100

    removed 10_expressframework

--- nodejs/10_expressframework/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* ALL about page. */
-router.all('/about', function(req, res, next) {
-  res.render('about');
-});
-
-module.exports = router;
--- nodejs/10_expressframework/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* ALL home page. */
-router.all('/', function(req, res, next) {
-  res.render('index');
-});
-
-module.exports = router;
